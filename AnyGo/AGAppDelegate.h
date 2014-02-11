bbd4a9207dacdeb2e28c947e353f6f6e2ebbd13c
WingleWong <huangyunming@verycd.com>
2014-02-11 19:56:30 +0800

    Initial Commit

---
+++ AnyGo/AGAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AGAppDelegate.h
+//  AnyGo
+//
+//  Created by WingleWong on 14-2-11.
+//  Copyright (c) 2014年 WingleWong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AGAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
