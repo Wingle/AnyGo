3defbf841050feb9b856c0af535cdee6997cf8d0
WingleWong <huangyunming@verycd.com>
2014-02-12 14:12:29 +0800

    推荐位View编写。

--- AnyGo/AGAppDelegate.m
+++ AnyGo/AGAppDelegate.m
@@ -7,6 +7,10 @@
 //
 
 #import "AGAppDelegate.h"
+#import "PBFlatSettings.h"
+#import "AGTabBarController.h"
+#import "AGNavigationController.h"
+#import "AGViewController.h"
 
 @implementation AGAppDelegate
 
@@ -15,6 +19,17 @@
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     // Override point for customization after application launch.
     self.window.backgroundColor = [UIColor whiteColor];
+    
+//    if ([[[UIDevice currentDevice] systemVersion] floatValue] >= 7) {
+//        [application setStatusBarStyle:UIStatusBarStyleLightContent];
+//        self.window.clipsToBounds =YES;
+//        self.window.frame =  CGRectMake(0,20,self.window.frame.size.width,self.window.frame.size.height-20);
+//        
+//        //Added on 19th Sep 2013
+//        self.window.bounds = CGRectMake(0, 20, self.window.frame.size.width, self.window.frame.size.height);
+//    }
+    [self buildViews];
+    
     [self.window makeKeyAndVisible];
     return YES;
 }
@@ -44,6 +59,30 @@
 - (void)applicationWillTerminate:(UIApplication *)application
 {
     // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+}
+
+#pragma mark - Custom Methods
+- (void)buildViews {
+    AGViewController *viewController0 = [[AGViewController alloc] init];
+    AGNavigationController *navigationController0 = [[AGNavigationController alloc] initWithRootViewController:viewController0];
+    
+    AGViewController *viewController1 = [[AGViewController alloc] init];
+    AGNavigationController *navigationController1 = [[AGNavigationController alloc] initWithRootViewController:viewController1];
+    
+    AGViewController *viewController2 = [[AGViewController alloc] init];
+    AGNavigationController *navigationController2 = [[AGNavigationController alloc] initWithRootViewController:viewController2];
+    
+    AGViewController *viewController3 = [[AGViewController alloc] init];
+    AGNavigationController *navigationController3 = [[AGNavigationController alloc] initWithRootViewController:viewController3];
+    
+    AGTabBarController *tabBarContrller = [[AGTabBarController alloc] init];
+    tabBarContrller.viewControllers = @[navigationController0, navigationController1, navigationController2, navigationController3];
+    
+    self.window.rootViewController = tabBarContrller;
+    
+    [[PBFlatSettings sharedInstance] navigationBarApperance];
+    
+    
 }
 
 @end
--- AnyGo/Base/AGNavigationController.m
+++ AnyGo/Base/AGNavigationController.m
@@ -27,6 +27,7 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view.
+    self.edgesForExtendedLayout = UIRectEdgeNone;
 }
 
 - (void)didReceiveMemoryWarning
